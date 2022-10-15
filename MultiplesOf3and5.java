05d4be8c97315a384de06385d4fe898a644281be
Shenbaga Maharaja <76839423+shenbaga-maharaja@users.noreply.github.com>
2022-10-15 19:13:06 +0530

    Update MultiplesOf3and5.java

--- MultiplesOf3and5.java
+++ MultiplesOf3and5.java
@@ -1,18 +1,17 @@
 /**
-	* @shenbaga-maharaja
-  	* Coded To Code
+	PROBLEM STATEMNT
 	* Java Program To Find Multiples Of 3 And 5
 	* If We List All The Natural Numbers Below 10 That Are Multiples Of 3 Or 5, 
 		We Get 3, 5, 6 And 9. The Sum Of These Multiples Is 23.
-
 	* Find The Sum Of All The Multiples Of 3 Or 5 Below The Provided Parameter Value Number.
-
 	* Test Cases
 		multiplesOf3and5(10) should return a number.
 		multiplesOf3and5(49) should return 543.
 		multiplesOf3and5(1000) should return 233168.
 		multiplesOf3and5(8456) should return 16687353.
 		multiplesOf3and5(19564) should return 89301183.
+	* @shenbaga-maharaja
+  	* Coded To Code
 **/
 
 import java.util.Scanner;
